20cf8c1266a8c5ea7d8a7de1cf685a04b82f08ca
Jermaine <127801090+JermaineCalip@users.noreply.github.com>
2024-05-08 01:00:31 +1200

    Debugging
    Switching from Visual Studio to PyCharm for easier coding

--- GDDA612/Acitivity 2/Activity-2.py
+++ GDDA612/Acitivity 2/Activity-2.py
@@ -1,1 +1,10 @@
+# Import necessary packages
+import pandas as pd
+import numpy as np
+
+# Reading the csv file
+DBOlympics = pd.read_csv(
+    'C:\Users\User\OneDrive\Desktop\NZSE Files\GDDA-Projects\GDDA612\Acitivity 2\BL-Flickr-Images-Book.csv'
+)
+
 
