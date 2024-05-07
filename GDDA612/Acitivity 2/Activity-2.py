b8748f8d5bc84c091ccf670b1fe5d9a3ce00cf80
Jermaine <127801090+JermaineCalip@users.noreply.github.com>
2024-05-08 01:02:33 +1200

    Test

--- GDDA612/Acitivity 2/Activity-2.py
+++ GDDA612/Acitivity 2/Activity-2.py
@@ -3,8 +3,6 @@ import pandas as pd
 import numpy as np
 
 # Reading the csv file
-DBOlympics = pd.read_csv(
-    'C:\Users\User\OneDrive\Desktop\NZSE Files\GDDA-Projects\GDDA612\Acitivity 2\BL-Flickr-Images-Book.csv'
-)
+DBOlympics = pd.read_csv('olympics.csv')
 
 
