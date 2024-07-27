84ad84218b82cb3343a19e5a460c732181b8fb6c
Jermaine <127801090+JermaineCalip@users.noreply.github.com>
2024-07-28 01:31:32 +1200

    Task 1 Assessment 2 of GD604
    Finished Task 1 on A2 GD604

--- GD604/Assessment 2.py
+++ GD604/Assessment 2.py
@@ -2,34 +2,61 @@
 import pandas as pd
 
 # This is to load the dataset into a DataFrame
-df = pd.read_csv("Sales_Sample_Public_Dataset.csv", encoding="ISO-8859-1")
+df = pd.read_csv("Retail Dataset.csv")
 
-# This is to show few rows in the DataFrame
-print(df.head(5).to_string())
+# This is to show first 5 rows in the DataFrame
+print(df.head().to_string())
+
+# This is to show last 5 rows in the DataFrame
+print(df.tail(10).to_string())
 
 # This is to show the total numbers of missing values
 print(df.isnull().sum())
+# This is to show each column's data type
+print(df.dtypes)
 
-# # This is to solve the missing values found in categorical columns using mode imputation.
-# categorical_columns = df.select_dtypes(include=['object']).columns
-# df[categorical_columns] = df[categorical_columns].fillna(df[categorical_columns].mode().iloc[0])
-# # This is to display missing values after mode imputation for verify.
-# # print(df.isnull().sum(),"\n")
-#
-# # This is to solve the missing values found in numerical columns using mean imputation.
-# numerical_columns = df.select_dtypes(include=['int64', 'float64']).columns
-# df[numerical_columns] = df[numerical_columns].fillna(df[numerical_columns].mean())
-# # This is to display missing values after mean imputation for verify.
-# # print(df.isnull().sum(),"\n")
-#
-# print(df.isnull().sum().sum())
-#
-# df.to_csv('test.csv', index=False)
-#
-# df = pd.read_csv('test.csv')
-# print (df.head(5).to_string())
-#
-# print(df.dtypes)
+# Dealing with Missing Values
+# Using Mean Imputation
+mean = ['order_price', 'order_total', 'distance_to_nearest_warehouse']
+df[mean] = df[mean].fillna(df[mean].mean())
 
-# sns.boxplot(df.)
-# sns.distplot(df.salary)
+# Dropping Unnecessary Column with Missing Values
+drop = ['customer_lat', 'customer_long']
+df.drop(drop, axis=1, inplace=True)
+df.dropna(subset=['is_happy_customer'], inplace=True)
+
+# Filling NaN with Values
+fill = ['season', 'nearest_warehouse']
+df[fill] = df[fill].fillna('Unknown')
+print(df.isnull().sum())
+
+# Ascending Order
+df_asc_sorted = df.sort_values(by='order_total')
+print(df_asc_sorted.head().to_string())
+
+# Descending Order
+df_desc_sorted = df.sort_values(by='order_total', ascending=False)
+print(df_desc_sorted.head().to_string())
+
+# Filtering
+df_filtered = df[df['delivery_charges'] > 100]
+print(df_filtered.head().to_string())
+
+# New Column
+df['date'] = pd.to_datetime(df['date'])
+df['order_month'] = df['date'].dt.month
+monthly_counts = df.groupby('order_month').size()
+print(monthly_counts)
+
+# Aggregate
+df_aggregated = df.groupby('nearest_warehouse').agg({'order_total': 'sum'})
+print(df_aggregated)
+
+
+
+
+
+
+
+
+
