bea03c920b3dd6a20ed973770fe97c0a246264a8
Jermaine <127801090+JermaineCalip@users.noreply.github.com>
2024-07-30 14:18:46 +1200

    Task 3 Completion
    Code for Task 3 Completion

--- GD604/Assessment 2.py
+++ GD604/Assessment 2.py
@@ -1,6 +1,11 @@
 # Import necessary packages
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from scipy.stats import ttest_ind
 
+# Task 1
 # This is to load the dataset into a DataFrame
 df = pd.read_csv("Retail Dataset.csv")
 
@@ -44,19 +49,61 @@ print(df_filtered.head().to_string())
 
 # New Column
 df['date'] = pd.to_datetime(df['date'])
-df['order_month'] = df['date'].dt.month
-monthly_counts = df.groupby('order_month').size()
+df['order_per_month'] = df['date'].dt.month
+monthly_counts = df.groupby('order_per_month').size()
 print(monthly_counts)
 
 # Aggregate
 df_aggregated = df.groupby('nearest_warehouse').agg({'order_total': 'sum'})
 print(df_aggregated)
 
+# Task 2
+# Summary Statistics
+categorical_columns = df.select_dtypes(include='object')
+print(categorical_columns.describe().to_string())
 
+# Correlational
+df_encoded = pd.get_dummies(df, columns=['nearest_warehouse', 'season', 'is_happy_customer'])
+numerical_columns = df_encoded.select_dtypes(include=['int64', 'float64', 'bool'])
+df_corr = numerical_columns.corr()
 
+# Visualization
+plt.figure(figsize=(14, 12))
+sns.heatmap(df_corr, annot=True, cmap='coolwarm')
+plt.title('Correlation Matrix Heatmap')
+plt.show()
 
+# Exporting CSV
+df.to_csv('cleaned_retail_dataset.csv', index=False)
 
+# Visualization (Box Plot)
+plt.figure(figsize=(14, 8))
+sns.boxplot(data=df, x='season', y='order_total')
+plt.title('Total Order per Season')
+plt.xlabel('Season')
+plt.ylabel('Total Order')
+plt.tight_layout()
+plt.show()
 
+# Visualization (Bar Plot)
+sales = df.groupby('nearest_warehouse')['order_total'].sum().reset_index()
+sorted = sales.sort_values(by='order_total', ascending=False)
+plt.figure(figsize=(14, 8))
+sns.barplot(data=sorted, x='nearest_warehouse', y='order_total')
+plt.title('Total Order per Nearest Warehouse')
+plt.xlabel('Nearest Warehouse')
+plt.ylabel('Order Total')
+plt.tight_layout()
+plt.show()
 
+# Inferential Analysis
+alpha = 0.05
+t_stat, p_val = ttest_ind(df['order_total'], df['order_price'])
+print(f"T-statistic: {t_stat}")
+print(f"P-value: {p_val}")
+if p_val < alpha:
+    print("Reject the null hypothesis: There is a significant difference between order_total and order_price.")
+else:
+    print("Fail to reject the null hypothesis: There is no significant difference between order_total and order_price.")
 
 
